4a8f2b022a7fd5a4b237ab5db6ec08b042848884
Fabian Zapata <fabian@g12media.net>
2017-06-13 11:39:53 -0500

    Update Login frontend

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -22,36 +22,7 @@
 <!-- Topbar header - style you can find in pages.scss -->
 <!-- ============================================================== -->
 <nav class="navbar navbar-default navbar-static-top m-b-0">
-    <div class="navbar-header">
-        <div class="pl-50">
-            <!-- Logo -->
-            <a class="logo" href="index.html">
-              <b>
-                <img src="{{URL::asset('assets/plugins/images/arm_logo2.png')}}" alt="home" class="light-logo" />
-             </b>
-            </a>
-        </div>
-        <!-- /Logo -->
-        <ul class="nav navbar-top-links navbar-right pull-right pr-50">
-          <li class="dropdown">
-              <a class="dropdown-toggle profile-pic" data-toggle="dropdown" href="#"><b class="hidden-xs"><i class="fa fa-language"></i>  English </b><span class="caret"></span> </a>
-              <ul class="dropdown-menu dropdown-user animated flipInY">
-                  <li><a href="#" class="text-right"> Spanish</a></li>
-                  <li><a href="#" class="text-right"> Germany</a></li>
-              </ul>
-              <!-- /.dropdown-language -->
-          </li>
-            <li class="dropdown">
-                <a class="dropdown-toggle profile-pic" data-toggle="dropdown" href="#"><b class="hidden-xs"><i class="fa fa-user"></i>  Login </b><span class="caret"></span> </a>
-                <ul class="dropdown-menu dropdown-user animated flipInY">
-                    <li><a href="#" class="text-right"> User Login</a></li>
-                    <li><a href="#" class="text-right"> Admin Login</a></li>
-                </ul>
-                <!-- /.dropdown-login -->
-            </li>
-            <!-- /.dropdown -->
-        </ul>
-    </div>
+
     <!-- /.navbar-header -->
     <!-- /.navbar-top-links -->
     <!-- /.navbar-static-side -->
